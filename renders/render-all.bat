7e3f1abfc734b3a033278adcb1978c1c304734c2
Jota Lanusse <jotalanusse@gmail.com>
2021-05-15 19:02:49 -0300

    Added render config

---
+++ renders/render-all.bat
@@ -0,0 +1,5 @@
+C:\Users\jotal\Desktop\overviewer-0.16.12\overviewer.exe --config=season3-bcc-server-config
+C:\Users\jotal\Desktop\overviewer-0.16.12\overviewer.exe --config=season4-bcc-server-config
+C:\Users\jotal\Desktop\overviewer-0.16.12\overviewer.exe --config=season5-bcc-server-config
+
+pause
