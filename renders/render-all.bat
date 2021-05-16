be3cebda3b24faac5e8d156442408991b911b0c2
Jota Lanusse <jotalanusse@gmail.com>
2021-05-16 05:41:19 -0300

    Edited the render configuration

--- renders/render-all.bat
+++ renders/render-all.bat
@@ -1,3 +1,6 @@
 C:\Users\jotal\Desktop\overviewer-0.16.12\overviewer.exe --config=season-2-config
+C:\Users\jotal\Desktop\overviewer-0.16.12\overviewer.exe --config=season-3-config
+C:\Users\jotal\Desktop\overviewer-0.16.12\overviewer.exe --config=season-4-config
+C:\Users\jotal\Desktop\overviewer-0.16.12\overviewer.exe --config=season-5-config
 
 pause
